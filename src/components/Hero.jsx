4c10c0248fb6aab42c07d5f294b32ae87238e13f
ridoy113 <almamounhridoy113@gmail.com>
2022-06-27 18:43:08 +0600

    navbar finish

---
+++ src/components/Hero.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import {
+    CloudUpLoadIcon,
+    DatabaseIcon,
+    PaperAirplaneIcon,
+    ServerIcon
+} from '@heroicons/react/solid'
+
+
+
+const Hero = () => {
+    return (
+        <div>Hero</div>
+    )
+}
+
+export default Hero
