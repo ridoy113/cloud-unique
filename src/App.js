8981264f03c07530938d6a4a3113c09611dafaa6
ridoy113 <almamounhridoy113@gmail.com>
2022-06-28 14:53:24 +0600

    allinone done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import About from "./components/About";
+import AllInOne from "./components/AllInOne";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Support from "./components/Support";
@@ -11,6 +12,7 @@ function App() {
       <Hero />
       <About />
       <Support />
+      <AllInOne/>
 
     </>
   );
--- src/components/Support.jsx
+++ src/components/Support.jsx
@@ -8,7 +8,7 @@ import suppoortImg from '../assets/support.jpg'
 
 const Support = () => {
     return (
-        <div className='w-full h-screen mt-24 '>
+        <div className='w-full mt-24 '>
             <div className='w-full  h-[700px]  bg-gray-900/90 absolute'>
                 <img className='w-full h-full object-cover mix-blend-overlay' src={suppoortImg} alt="/" />
             </div>
